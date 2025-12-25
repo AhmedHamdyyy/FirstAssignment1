980dd8299f17869cf47f1c9d4cc8b7ae82e4854b
AhmedHamdyyy <94721322+AhmedHamdyyy@users.noreply.github.com>
2025-12-25 16:19:58 +0200

    Fix: make footer responsive

--- src/app/footer/footer.ts
+++ src/app/footer/footer.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -7,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrl: './footer.css',
 })
 export class Footer {
+
+  @ViewChild('footer') footer !: ElementRef<HTMLDivElement>;
+  @HostListener('window:scroll',['$event']) onScrolling(event:any){
+  if(window.scrollY > 0){
+        this.footer.nativeElement.classList.add('py-0');
+        this.footer.nativeElement.classList.remove('pt-2');
+  }else{
+        this.footer.nativeElement.classList.add('pt-2');
+        this.footer.nativeElement.classList.remove('py-0');
+  }
+}
 
 }
