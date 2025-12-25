bb76c94f162fc170a092e6c169fd9a92f18250d2
AhmedHamdyyy <94721322+AhmedHamdyyy@users.noreply.github.com>
2025-12-25 15:58:07 +0200

    upload Responsiveness

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { RouterLink , RouterLinkActive } from '@angular/router';
 
 @Component({
@@ -8,5 +8,16 @@ import { RouterLink , RouterLinkActive } from '@angular/router';
   styleUrl: './navbar.css',
 })
 export class Navbar {
+
+  @ViewChild('navbar') navbar !: ElementRef<HTMLDivElement>;
+  @HostListener('window:scroll',['$event']) onScrolling(event:any){
+  if(window.scrollY > 0){
+        this.navbar.nativeElement.classList.add('py-0');
+        this.navbar.nativeElement.classList.remove('py-4');
+  }else{
+        this.navbar.nativeElement.classList.add('py-4');
+        this.navbar.nativeElement.classList.remove('py-0');
+  }
+}
 
 }
